1f959642d54a25978b9cca1c0594e88b0612d248
xysticus <alexandrebenoit@live.com>
2022-05-06 14:43:18 +0200

    version paramétrique pas mal

--- tutocadquery.py
+++ tutocadquery.py
@@ -4,19 +4,25 @@ from cadquery import exporters
 from cadquery import Edge
 from cadquery import Wire
 from cadquery import Face
-from cadquery import Shell
-from cadquery import Solid
 from cadquery import Vector
 from cadquery import Workplane
 
-
 racine = "./dmccooey.com/polyhedra/"
 nom_polyedre= "TruncatedIcosidodecahedron"
-trou1=2.8
-trou2=2.4
-epaisseur=0.15
-largeur1=0.5
-largeur=2.2
+marge = [0.0]*12
+marge[10] = 0.0
+marge[6] = 0.2
+marge[4] = 0
+epaisseur = [0.0]*12
+epaisseur[10] = 0.3
+epaisseur[6] = 0.15
+epaisseur[4] = 0.15
+largeur = [0.0]*12
+largeur[10] = 0
+largeur[6] = 2.4
+largeur[4] = 2.5
+sphere_ext_ry = 4.6
+sphere_int_ry = 4.45
 
 (sommets, les_faces) = aspiro(racine + nom_polyedre + ".html")
 
@@ -25,7 +31,7 @@ les_faces = [ l + [l[0]] for l in les_faces ]
 print(sommets)
 print(les_faces)
 
-def Polyedre(s=1):
+def Polyedre():
    
     le_tout = Workplane()
 
@@ -41,20 +47,21 @@ def Polyedre(s=1):
         wire = Wire.combine(lines)
         une_face = Face.makeFromWires(*wire)
 
-        if len(ixs)==11:
-            pass
-            #dessus = Workplane(une_face).faces().workplane().add(une_face).wires().toPending().\
-            #    offset2D(largeur1,"arc").extrude(epaisseur).faces().end().workplane().circle(trou1).cutThruAll()
-        elif len(ixs)==7:
-           dessus = Workplane(une_face).faces().workplane().add(une_face).wires().toPending().\
-           offset2D(largeur,"arc").extrude(epaisseur).faces().end().workplane().circle(trou2).cutThruAll()
-           le_tout = le_tout.add(dessus)
+        nb_faces = len(ixs) - 1
 
-        elif len(ixs)==5:
-           dessus = Workplane(une_face).faces().workplane().add(une_face).wires().toPending().\
-           offset2D(largeur,"arc").extrude(epaisseur)
-           le_tout = le_tout.add(dessus)
+        if largeur[nb_faces] > 0:
+            if marge[nb_faces] > 0:
+                    dessus = Workplane(une_face).faces().workplane().add(une_face).wires().toPending().\
+                    offset2D(largeur[nb_faces],"arc").extrude(epaisseur[nb_faces]).faces().end().workplane().\
+                    add(une_face).wires().toPending().offset2D(largeur[nb_faces]-marge[nb_faces],"arc").extrude(epaisseur[nb_faces],combine="cut")
+            else:   
+                    dessus = Workplane(une_face).faces().workplane().add(une_face).wires().toPending().\
+                    offset2D(largeur[nb_faces],"arc").extrude(epaisseur[nb_faces])
 
-    return le_tout
+            le_tout = le_tout.union(dessus)
 
-exporters.export(Polyedre(),"./stl/" + nom_polyedre + ".stl",tolerance=0.5)
+    sphere_ext = cq.Workplane().sphere(sphere_ext_ry)
+    sphere_int = cq.Workplane().sphere(sphere_int_ry)
+    return le_tout.union(sphere_ext).cut(sphere_int).workplane().circle(.3).cutThruAll()
+
+exporters.export(Polyedre(),"./stl/" + nom_polyedre + ".stl",tolerance=0.8)
